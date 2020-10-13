ab9ce885060acb25085b39d53501f9206735f678
aronjt <jtelekiaron@gmail.com>
2020-10-13 14:40:48 +0200

    evensum

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -43,4 +43,13 @@ public class Main {
             }
         }return counter;
     }
+    public static int sumEven (int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
 }
