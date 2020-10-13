0ec76360f6d18728aec445cfa1c7e5afa114d1b7
Tuzson <kecseyt@gmail.com>
2020-10-13 14:44:50 +0200

    oddsum

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -52,4 +52,12 @@ public class Main {
         }
         return sum;
     }
+    public static int oddSum(int [] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0){
+                sum+=arr[i];
+            }
+        }return sum;
+    }
 }
