4c4e6e92232f4e82d284d3c0487af11cdd46ea25
aronjt <jtelekiaron@gmail.com>
2020-10-13 14:03:07 +0200

    minimum

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -8,4 +8,13 @@ public class Main {
             array[i] = (int) (Math.random()*100);
         }
     }
+    public static int minimum (int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 }
