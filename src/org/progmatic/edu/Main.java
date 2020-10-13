ff9edf2e0f9d09446e7c2fa18a2bc6b9cc889a4d
Tuzson <kecseyt@gmail.com>
2020-10-13 14:32:11 +0200

    max

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -26,4 +26,13 @@ public class Main {
         }
         return evenNums;
     }
+    public static int max (int[]arr ){
+        int max = int[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+            }
+
+        }return max;
+    }
 }
