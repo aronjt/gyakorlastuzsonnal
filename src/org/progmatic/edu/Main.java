4fd3e9797188c67b6c3d83d56e070b11c3dd36e3
aronjt <jtelekiaron@gmail.com>
2020-10-13 14:00:35 +0200

    valtoztattam

--- src/org/progmatic/edu/Main.java
+++ src/org/progmatic/edu/Main.java
@@ -3,6 +3,9 @@ package org.progmatic.edu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random()*100);
+        }
     }
 }
